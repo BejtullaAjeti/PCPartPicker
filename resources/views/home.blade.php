e897024742fffaa20aebcc3de4e4bb5b32f398e5
lenarrt <lk29224@seeu.edu.mk>
2023-04-08 14:51:19 +0200

    PC-Build-updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if (session('error'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ session('error') }}
-                        </div>
-                    @endif
-                    <h3>Welcomee to the User Dashboard</h3>
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        @if (session('error'))
+                            <div class="alert alert-danger" role="alert">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+                        <h3>Welcomee to the User Dashboard</h3>
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div>
+                        <p><a class="btn btn-secondary" href="{{ url('/pc_part') }}" role="button">Build a PC</a></p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
