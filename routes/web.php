f473ef40e39a7deedb9b013bede13c8c38e24cb6
Bejtulla Ajeti <74537519+Bejtulla-2020@users.noreply.github.com>
2023-04-08 14:36:16 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/', function(){
 
 
 //Route::resource("/cpu", CpuController::class);
-//Route::resource("/gpu", GpuController::class);    
+//Route::resource("/gpu", GpuController::class);
 //Route::resource("/ram", RamController::class);
 //Route::resource('/tower', TowerController::class);
 //Route::resource('/cpu_cooler', CpuCoolerController::class);
@@ -51,8 +51,8 @@ Route::get('/admin/home/cpu', [App\Http\Controllers\CpuController::class, 'index
 Route::get('/admin/home/cpu/create', [App\Http\Controllers\CpuController::class, 'create'])->name('create.cpu')->middleware('is_admin');
 Route::post('/admin/home/cpu/store', [App\Http\Controllers\CpuController::class, 'store'])->name('store.cpu')->middleware('is_admin');
 Route::get('/cpu/details/{id}', [App\Http\Controllers\CpuController::class, 'show'])->name('show.cpu.details');
-Route::get('/cpu/edit/{id}', [App\Http\Controllers\CpuController::class, 'edit'])->name('edit.cpu')->middleware('is_admin');;
-Route::put('admin/home/cpu/{id}', [CpuController::class, 'update'])->name('cpu.update')->middleware('is_admin');;
+Route::get('/cpu/edit/{id}', [App\Http\Controllers\CpuController::class, 'edit'])->name('edit.cpu')->middleware('is_admin');
+Route::put('admin/home/cpu/{id}', [CpuController::class, 'update'])->name('cpu.update')->middleware('is_admin');
 Route::put('/admin/home/cpu/update', [App\Http\Controllers\CpuController::class, 'update'])->name('update.cpu')->middleware('is_admin');
 Route::delete('/admin/home/cpu/delete/{id}', [App\Http\Controllers\CpuController::class, 'destroy'])->name('delete.cpu')->middleware('is_admin');
 
@@ -61,8 +61,8 @@ Route::get('/admin/home/gpu', [App\Http\Controllers\GpuController::class, 'index
 Route::get('/admin/home/gpu/create', [App\Http\Controllers\GpuController::class, 'create'])->name('create.gpu')->middleware('is_admin');
 Route::post('/admin/home/gpu/store', [App\Http\Controllers\GpuController::class, 'store'])->name('store.gpu')->middleware('is_admin');
 Route::get('/gpu/details/{id}', [App\Http\Controllers\GpuController::class, 'show'])->name('show.gpu.details');
-Route::get('/gpu/edit/{id}', [App\Http\Controllers\GpuController::class, 'edit'])->name('edit.gpu')->middleware('is_admin');;
-Route::put('admin/home/gpu/{id}', [GpuController::class, 'update'])->name('gpu.update')->middleware('is_admin');;
+Route::get('/gpu/edit/{id}', [App\Http\Controllers\GpuController::class, 'edit'])->name('edit.gpu')->middleware('is_admin');
+Route::put('admin/home/gpu/{id}', [GpuController::class, 'update'])->name('gpu.update')->middleware('is_admin');
 Route::put('/admin/home/gpu/update', [App\Http\Controllers\GpuController::class, 'update'])->name('update.gpu')->middleware('is_admin');
 Route::delete('/admin/home/gpu/delete/{id}', [App\Http\Controllers\GpuController::class, 'destroy'])->name('delete.gpu')->middleware('is_admin');
 
@@ -71,8 +71,8 @@ Route::get('/admin/home/cpu_cooler', [App\Http\Controllers\CpuCoolerController::
 Route::get('/admin/home/cpu_cooler/create', [App\Http\Controllers\CpuCoolerController::class, 'create'])->name('create.cpu_cooler')->middleware('is_admin');
 Route::post('/admin/home/cpu_cooler/store', [App\Http\Controllers\CpuCoolerController::class, 'store'])->name('store.cpu_cooler')->middleware('is_admin');
 Route::get('/cpu_cooler/details/{id}', [App\Http\Controllers\CpuCoolerController::class, 'show'])->name('show.cpu_cooler.details');
-Route::get('/cpu_cooler/edit/{id}', [App\Http\Controllers\CpuCoolerController::class, 'edit'])->name('edit.cpu_cooler')->middleware('is_admin');;
-Route::put('admin/home/cpu_cooler/{id}', [CpuCoolerController::class, 'update'])->name('cpu_cooler.update')->middleware('is_admin');;
+Route::get('/cpu_cooler/edit/{id}', [App\Http\Controllers\CpuCoolerController::class, 'edit'])->name('edit.cpu_cooler')->middleware('is_admin');
+Route::put('admin/home/cpu_cooler/{id}', [CpuCoolerController::class, 'update'])->name('cpu_cooler.update')->middleware('is_admin');
 Route::put('/admin/home/cpu_cooler/update', [App\Http\Controllers\CpuCoolerController::class, 'update'])->name('update.cpu_cooler')->middleware('is_admin');
 Route::delete('/admin/home/cpu_cooler/delete/{id}', [App\Http\Controllers\CpuCoolerController::class, 'destroy'])->name('delete.cpu_cooler')->middleware('is_admin');
 
@@ -81,8 +81,8 @@ Route::get('/admin/home/motherboard', [App\Http\Controllers\MotherboardControlle
 Route::get('/admin/home/motherboard/create', [App\Http\Controllers\MotherboardController::class, 'create'])->name('create.motherboard')->middleware('is_admin');
 Route::post('/admin/home/motherboard/store', [App\Http\Controllers\MotherboardController::class, 'store'])->name('store.motherboard')->middleware('is_admin');
 Route::get('/motherboard/details/{id}', [App\Http\Controllers\MotherboardController::class, 'show'])->name('show.motherboard.details');
-Route::get('/motherboard/edit/{id}', [App\Http\Controllers\MotherboardController::class, 'edit'])->name('edit.motherboard')->middleware('is_admin');;
-Route::put('admin/home/motherboard/{id}', [MotherboardController::class, 'update'])->name('motherboard.update')->middleware('is_admin');;
+Route::get('/motherboard/edit/{id}', [App\Http\Controllers\MotherboardController::class, 'edit'])->name('edit.motherboard')->middleware('is_admin');
+Route::put('admin/home/motherboard/{id}', [MotherboardController::class, 'update'])->name('motherboard.update')->middleware('is_admin');
 Route::put('/admin/home/motherboard/update', [App\Http\Controllers\MotherboardController::class, 'update'])->name('update.motherboard')->middleware('is_admin');
 Route::delete('/admin/home/motherboard/delete/{id}', [App\Http\Controllers\MotherboardController::class, 'destroy'])->name('delete.motherboard')->middleware('is_admin');
 
@@ -91,8 +91,8 @@ Route::get('/admin/home/power_supply', [App\Http\Controllers\PowerSupplyControll
 Route::get('/admin/home/power_supply/create', [App\Http\Controllers\PowerSupplyController::class, 'create'])->name('create.power_supply')->middleware('is_admin');
 Route::post('/admin/home/power_supply/store', [App\Http\Controllers\PowerSupplyController::class, 'store'])->name('store.power_supply')->middleware('is_admin');
 Route::get('/power_supply/details/{id}', [App\Http\Controllers\PowerSupplyController::class, 'show'])->name('show.power_supply.details');
-Route::get('/power_supply/edit/{id}', [App\Http\Controllers\PowerSupplyController::class, 'edit'])->name('edit.power_supply')->middleware('is_admin');;
-Route::put('admin/home/power_supply/{id}', [PowerSupplyController::class, 'update'])->name('power_supply.update')->middleware('is_admin');;
+Route::get('/power_supply/edit/{id}', [App\Http\Controllers\PowerSupplyController::class, 'edit'])->name('edit.power_supply')->middleware('is_admin');
+Route::put('admin/home/power_supply/{id}', [PowerSupplyController::class, 'update'])->name('power_supply.update')->middleware('is_admin');
 Route::put('/admin/home/power_supply/update', [App\Http\Controllers\PowerSupplyController::class, 'update'])->name('update.power_supply')->middleware('is_admin');
 Route::delete('/admin/home/power_supply/delete/{id}', [App\Http\Controllers\PowerSupplyController::class, 'destroy'])->name('delete.power_supply')->middleware('is_admin');
 
@@ -101,8 +101,8 @@ Route::get('/admin/home/ram', [App\Http\Controllers\RamController::class, 'index
 Route::get('/admin/home/ram/create', [App\Http\Controllers\RamController::class, 'create'])->name('create.ram')->middleware('is_admin');
 Route::post('/admin/home/ram/store', [App\Http\Controllers\RamController::class, 'store'])->name('store.ram')->middleware('is_admin');
 Route::get('/ram/details/{id}', [App\Http\Controllers\RamController::class, 'show'])->name('show.ram.details');
-Route::get('/ram/edit/{id}', [App\Http\Controllers\RamController::class, 'edit'])->name('edit.ram')->middleware('is_admin');;
-Route::put('admin/home/ram/{id}', [RamController::class, 'update'])->name('ram.update')->middleware('is_admin');;
+Route::get('/ram/edit/{id}', [App\Http\Controllers\RamController::class, 'edit'])->name('edit.ram')->middleware('is_admin');
+Route::put('admin/home/ram/{id}', [RamController::class, 'update'])->name('ram.update')->middleware('is_admin');
 Route::put('/admin/home/ram/update', [App\Http\Controllers\RamController::class, 'update'])->name('update.ram')->middleware('is_admin');
 Route::delete('/admin/home/ram/delete/{id}', [App\Http\Controllers\RamController::class, 'destroy'])->name('delete.ram')->middleware('is_admin');
 
@@ -111,8 +111,8 @@ Route::get('/admin/home/storage', [App\Http\Controllers\StorageController::class
 Route::get('/admin/home/storage/create', [App\Http\Controllers\StorageController::class, 'create'])->name('create.storage')->middleware('is_admin');
 Route::post('/admin/home/storage/store', [App\Http\Controllers\StorageController::class, 'store'])->name('store.storage')->middleware('is_admin');
 Route::get('/storage/details/{id}', [App\Http\Controllers\StorageController::class, 'show'])->name('show.storage.details');
-Route::get('/storage/edit/{id}', [App\Http\Controllers\StorageController::class, 'edit'])->name('edit.storage')->middleware('is_admin');;
-Route::put('admin/home/storage/{id}', [StorageController::class, 'update'])->name('storage.update')->middleware('is_admin');;
+Route::get('/storage/edit/{id}', [App\Http\Controllers\StorageController::class, 'edit'])->name('edit.storage')->middleware('is_admin');
+Route::put('admin/home/storage/{id}', [StorageController::class, 'update'])->name('storage.update')->middleware('is_admin');
 Route::put('/admin/home/storage/update', [App\Http\Controllers\StorageController::class, 'update'])->name('update.storage')->middleware('is_admin');
 Route::delete('/admin/home/storage/delete/{id}', [App\Http\Controllers\StorageController::class, 'destroy'])->name('delete.storage')->middleware('is_admin');
 
@@ -121,8 +121,8 @@ Route::get('/admin/home/tower', [App\Http\Controllers\TowerController::class, 'i
 Route::get('/admin/home/tower/create', [App\Http\Controllers\TowerController::class, 'create'])->name('create.tower')->middleware('is_admin');
 Route::post('/admin/home/tower/store', [App\Http\Controllers\TowerController::class, 'store'])->name('store.tower')->middleware('is_admin');
 Route::get('/tower/details/{id}', [App\Http\Controllers\TowerController::class, 'show'])->name('show.tower.details');
-Route::get('/tower/edit/{id}', [App\Http\Controllers\TowerController::class, 'edit'])->name('edit.tower')->middleware('is_admin');;
-Route::put('admin/home/tower/{id}', [TowerController::class, 'update'])->name('tower.update')->middleware('is_admin');;
+Route::get('/tower/edit/{id}', [App\Http\Controllers\TowerController::class, 'edit'])->name('edit.tower')->middleware('is_admin');
+Route::put('admin/home/tower/{id}', [TowerController::class, 'update'])->name('tower.update')->middleware('is_admin');
 Route::put('/admin/home/tower/update', [App\Http\Controllers\TowerController::class, 'update'])->name('update.tower')->middleware('is_admin');
 Route::delete('/admin/home/tower/delete/{id}', [App\Http\Controllers\TowerController::class, 'destroy'])->name('delete.tower')->middleware('is_admin');
 
