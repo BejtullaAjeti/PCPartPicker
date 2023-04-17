8bcbd58703baae1a4b07eeca3ac66f2822336205
lumia <la29365@seeu.edu.mk>
2023-04-17 18:37:56 +0200

    Worked on the User Interface

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
-
-
-                     <div class="card-body">
+                    <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
@@ -20,12 +18,18 @@
                             </div>
                         @endif
                         <h3>Welcome to the User Dashboard</h3>
-                        {{ __('You are logged in!') }}
+                        {{ __('You are now logged in and are able to build your own PC!') }}
                     </div>
-                    <div>
-                        <p><a class="btn btn-secondary" href="{{ url('/pc_part') }}" role="button">Build a PC</a></p>
+                    <div style="text-align: center">
+                        <p><a class="btn btn-secondary" style="text-align: center" href="{{ url('/pc_part') }}" role="button">Build a PC</a></p>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+                <img src="{{ asset('img/image3.jpg') }}" alt="Image 2" style="width: 100%;">
             </div>
         </div>
     </div>
